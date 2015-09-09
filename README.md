08851ef7d0fe433bd47628bd85e366f4e2a26805
Dave Caputo <davidmcaputo@gmail.com>
2015-09-09 11:10:06 -0700

    Added merchant input and file creation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # merchant-parser
-Simple Python script to parse customer emails for a particular merchant from a large spreadsheet.
+Simple Python script to pull customer emails for specific merchants.
+
+Type a merchant name and a new csv containing all customers from that merchant will be created. The file contains customers first names, emails, last visit, and total visits. 
--- merchParse.py
+++ merchParse.py
@@ -1,11 +1,14 @@
 # Simple CSV Merchant Parser
 # Written by David Caputo
 
+import os
 import csv
 
 # Merchant to search on
-merchant = "EVL PDX"
+print "\n"
+merchant = raw_input("Enter merchant name: ")
 
+num_emails = 0
 
 # Row 3 = Merchant name
 # Row 4 = Customer First Name
@@ -14,9 +17,12 @@ merchant = "EVL PDX"
 # Row 8 = Last Visit
 # Row 9 = Visit Count
 
+filename = merchant + ".csv"
+
+
 f = open('CustomerRelationship-20150819.csv')
 
-emails_f = csv.writer(open("emails.csv", "wb"))
+emails_f = csv.writer(open(filename, "wb+"))
 emails_f.writerow(["First Name", "Email", "Last Visit", "Visit Count"])
 
 csv_f = csv.reader(f)
@@ -24,5 +30,11 @@ csv_f = csv.reader(f)
 for row in csv_f:
 	if row[3] == merchant and row[6] != "":
 		emails_f.writerow([row[4], row[6], row[8], row[9]])
+		num_emails += 1
+if num_emails > 0:
+	print "File created with", num_emails, "customer emails. \n"
+else:
+	os.remove(filename)
+	print "Mercant not found."
 
 f.close()
